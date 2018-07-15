dbf6cbbecad6f58ff916d2023cb11db2c21f9bfe
Daniel <daniel.waesch@enersis.de>
2018-07-15 13:14:33 +0200

    First commit

---
+++ Testing/Neuralo.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Neuralo():
+    def __init__(self):
+        self.hello = 'Hell World'
+
+
+    def greeting(self):
+        return print("Was geht aber hier "+ self.hello)
+
+
+
+nn = Neuralo()
+
+nn.greeting()
